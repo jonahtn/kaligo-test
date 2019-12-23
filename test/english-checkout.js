4eb8ef09e1a11782bb6846ae69a1ce95b53d3620
Theodorus Nugraha <theodorus.nugraha@shopee.com>
2019-12-23 16:03:25 +0700

    Revert "Revert "fix english checkout and add elements""
    This reverts commit bb697cb547b95a0b91a17827e1f610d1bbe336d0.

--- test/english-checkout.js
+++ test/english-checkout.js
@@ -5,24 +5,24 @@ const configElement = new Elements()
 var pathJson = "../data/englishData.json"
 var dataJson = require(pathJson)
 
-fixture`Test Add Quest Event Stamp`
+fixture`Test English Checkout`
     .page`https://www.kaligo.com/`
 
-// test('Book a hotel without a destination', async t => {
-//     await t
-//         .resizeWindow(1500, 800)
-//         .click(configElement.searchHotel)
-//         .expect(configElement.errorNoDestination.visible).ok()
-// })
+test('Book a hotel without a destination', async t => {
+    await t
+        .resizeWindow(1500, 800)
+        .click(configElement.searchHotel)
+        .expect(configElement.errorNoDestination.visible).ok()
+})
 
-test('Book a hotel', async t => {
+test('Book a hotel in English Language', async t => {
     await t
         .resizeWindow(1500, 800)
         .click(configElement.destination)
         .pressKey('S i n g a p o r e')
-        .wait(1000)
-        .pressKey('enter')
+        .click(configElement.chooseFirst)
         .click(configElement.searchHotel)
+        .click(configElement.ratingFilter5)
         .click(configElement.selectFirstHotel)
         .click(configElement.seeRoom)
         .click(configElement.selectRoom)
@@ -50,5 +50,18 @@ test('Book a hotel', async t => {
         .pressKey('S i n g a p o r e')
         .pressKey('enter')
         .click(configElement.checkboxTC)
+})
 
+test("User didn't input required fields", async t => {
+    await t
+        .resizeWindow(1500, 800)
+        .click(configElement.destination)
+        .pressKey('S i n g a p o r e')
+        .click(configElement.chooseFirst)
+        .click(configElement.searchHotel)
+        .click(configElement.selectFirstHotel)
+        .click(configElement.seeRoom)
+        .click(configElement.selectRoom)
+        .click(configElement.confirmBooking)
+        .expect(configElement.errorRequired.visible).ok()
 })
